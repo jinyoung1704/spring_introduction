00039e7e7c8616c91b5433e0d59adbeea5963a6f
jinyoung1704 <jinyoung1704@naver.com>
2023-11-06 14:23:39 +0900

    회원 서비스 개발

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,50 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*
+     * 회원가입
+     * */
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원 x
+        validateDuplicateMember(member); // 메소드 분리 ctrl + alt + M
+
+        /* result 의 반환값이 Optional<Member> 이므로 위에처럼 써도 됨
+        *   Optional<Member> result = memberRepository.findByName(member.getName()); //반환값 만들어줌 : ctrl + alt + v
+            result.ifPresent(m->{
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+            });
+        *
+        * */
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()) //반환값 만들어줌 : ctrl + alt + v
+            .ifPresent(m->{
+                throw new IllegalStateException("이미 존재하는 회원입니다");
+            });
+    }
+
+    /*
+    * 전체 회원 조회
+    * */
+    public List<Member> findMessage(){
+        return  memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
+
